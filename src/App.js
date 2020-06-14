5e9480e5772e19124422333ff02f54876543f0fb
chenMM <1197729488@qq.com>
2020-06-14 19:02:40 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,64 @@
-import React from 'react';
-import './App.css';
-import {Link, Route} from 'react-router-dom'
+import React from "react";
 
-import AntdButton from './views/AntdButton/index'
+import {Layout, Menu} from 'antd';
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from '@ant-design/icons';
+import './App.css'
 
-function App() {
-  return (
-      <div className="App">
-        <nav>
-          <Link to="/antd-button">antd-button</Link>
-        </nav>
-        <div>
-          <Route path='/antd-button' component={AntdButton}/>
-        </div>
-      </div>
-  );
+const {Header, Sider, Content} = Layout;
+
+export default class App extends React.Component {
+  state = {
+    collapsed: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
+
+  render() {
+    return (
+        <Layout style={{minHeight: '100vh'}}>
+          <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+            <div className="logo"/>
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+              <Menu.Item key="1" icon={<UserOutlined/>}>
+                nav 1
+              </Menu.Item>
+              <Menu.Item key="2" icon={<VideoCameraOutlined/>}>
+                nav 2
+              </Menu.Item>
+              <Menu.Item key="3" icon={<UploadOutlined/>}>
+                nav 3
+              </Menu.Item>
+            </Menu>
+          </Sider>
+          <Layout className="site-layout">
+            <Header className="site-layout-background" style={{padding: 0}}>
+              {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                className: 'trigger',
+                onClick: this.toggle,
+              })}
+            </Header>
+            <Content
+                className="site-layout-background"
+                style={{
+                  margin: '24px 16px',
+                  padding: 24,
+                  minHeight: 280,
+                }}
+            >
+              Content
+            </Content>
+          </Layout>
+        </Layout>
+    );
+  }
 }
-
-export default App;
